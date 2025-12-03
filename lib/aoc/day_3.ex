8f34f014c0eece989a90546653f315ff88883b17
Robert Ulejczyk <rulejczyk@gmail.com>
2025-12-03 10:27:34 +0100

    feat: day 3 part 2

--- lib/aoc/day_3.ex
+++ lib/aoc/day_3.ex
@@ -8,28 +8,46 @@ defmodule AOC.Day3 do
       |> Enum.map(&String.to_integer/1)
     end)
     |> Enum.map(fn bank ->
-      bank
-      |> Enum.with_index()
-      |> Enum.reduce(0, fn {first_digit, idx}, acc ->
-        last_digit =
-          bank
-          |> Enum.slice(idx + 1, length(bank))
-          |> case do
-            [] -> nil
-            candidates -> Enum.max(candidates)
-          end
-
-        number =
-          "#{first_digit}#{last_digit}"
-          |> String.to_integer()
-
-        if number > acc do
-          number
-        else
-          acc
-        end
-      end)
+      get_digits(bank, 2, [])
+      |> Enum.map(&Integer.to_string/1)
+      |> Enum.join("")
+      |> String.to_integer()
     end)
     |> Enum.sum()
+  end
+
+  def part_2() do
+    File.read!("data/day_3_2.txt")
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      line
+      |> String.split("", trim: true)
+      |> Enum.map(&String.to_integer/1)
+    end)
+    |> Enum.map(fn bank ->
+      get_digits(bank, 12, [])
+      |> Enum.map(&Integer.to_string/1)
+      |> Enum.join("")
+      |> String.to_integer()
+    end)
+    |> Enum.sum()
+  end
+
+  def get_digits(_candidates, 0, accumulator) do
+    accumulator
+  end
+
+  def get_digits(candidates, count, accumulator) do
+    valid_candidates =
+      Enum.slice(candidates, 0, length(candidates) - count + 1)
+
+    next_digit = Enum.max(valid_candidates)
+    next_idx = Enum.find_index(valid_candidates, &(&1 == next_digit))
+
+    next_candidates =
+      candidates
+      |> Enum.slice(next_idx + 1, length(candidates))
+
+    get_digits(next_candidates, count - 1, accumulator ++ [next_digit])
   end
 end
