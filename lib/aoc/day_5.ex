46a5df25aa03e7668303c53f9762f6b797786dc8
Robert Ulejczyk <rulejczyk@gmail.com>
2025-12-05 12:08:23 +0100

    feat: day 5 part 2

--- lib/aoc/day_5.ex
+++ lib/aoc/day_5.ex
@@ -1,4 +1,5 @@
 defmodule AOC.Day5 do
+  # completes in ~ 0.006 seconds
   def part_1() do
     [ranges, values] =
       File.read!("data/day_5_1.txt")
@@ -30,5 +31,94 @@ defmodule AOC.Day5 do
         acc
       end
     end)
+  end
+
+  # completes in ~ 0.000937 seconds
+  def part_2() do
+    [ranges, _values] =
+      File.read!("data/day_5_2.txt")
+      |> String.split("\n\n", trim: true)
+      |> Enum.map(fn half ->
+        half
+        |> String.split("\n", trim: true)
+      end)
+
+    ranges =
+      ranges
+      |> Enum.map(fn r ->
+        [first, last] = String.split(r, "-")
+
+        Range.new(
+          String.to_integer(first),
+          String.to_integer(last)
+        )
+      end)
+      |> Enum.sort_by(& &1.first, :asc)
+
+    %{visited: new_ranges} =
+      bfs(%{
+        candidates: ranges,
+        visited: []
+      })
+
+    new_ranges
+    |> Enum.map(&Range.size/1)
+    |> Enum.sum()
+  end
+
+  def bfs(state) when state.candidates == [] do
+    state
+  end
+
+  def bfs(state) do
+    {next, leftover_candidates} = List.pop_at(state.candidates, 0)
+
+    overlaping = Enum.find(state.visited, fn x -> not Range.disjoint?(next, x) end)
+    overlaping_idx = Enum.find_index(state.visited, &(&1 == overlaping))
+
+    {next_visited, next_candidates} =
+      if overlaping do
+        merged_range = merge_ranges(next, overlaping)
+
+        # requeue previous range in case it overlaps after merge
+        next_candidates =
+          if overlaping_idx > 0 do
+            previous = Enum.at(state.candidates, overlaping_idx - 1)
+            [previous | leftover_candidates]
+          else
+            leftover_candidates
+          end
+
+        next_visited = List.replace_at(state.visited, overlaping_idx, merged_range)
+
+        {next_visited, next_candidates}
+      else
+        next_visited = [next | state.visited]
+
+        {next_visited, leftover_candidates}
+      end
+
+    bfs(%{
+      visited: next_visited,
+      candidates: next_candidates
+    })
+  end
+
+  defp merge_ranges(r1, r2) do
+    next_first =
+      if r1.first < r2.first do
+        r1.first
+      else
+        r2.first
+      end
+
+    next_last =
+      if r1.last > r2.last do
+        r1.last
+      else
+        r2.last
+      end
+
+    Range.new(next_first, next_last)
   end
 end
